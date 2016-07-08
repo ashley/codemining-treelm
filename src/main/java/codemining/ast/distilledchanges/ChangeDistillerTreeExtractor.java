bb94815eab9fcfd518dd0ea2fbac24588367732a
Claire <claire.legoues@gmail.com>
2016-07-08 10:10:06 -0400

    check in class for potential tree extractor

---
+++ src/main/java/codemining/ast/distilledchanges/ChangeDistillerTreeExtractor.java
@@ -0,0 +1,49 @@
+package codemining.ast.distilledchanges;
+
+import java.io.File;
+import java.io.IOException;
+
+import codemining.ast.AbstractTreeExtractor;
+import codemining.ast.TreeNode;
+import codemining.languagetools.ITokenizer;
+import codemining.languagetools.ParseType;
+
+public class ChangeDistillerTreeExtractor extends AbstractTreeExtractor {
+
+	@Override
+	public String getCodeFromTree(TreeNode<Integer> tree) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode<Integer> getKeyForCompilationUnit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ITokenizer getTokenizer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode<Integer> getTree(File f) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode<Integer> getTree(String code, ParseType parseType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeToString getTreePrinter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
